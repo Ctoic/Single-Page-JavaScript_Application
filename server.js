46d988e70329f58264f0b4555496c56181baf3c9
Ctoic <i202612@nu.edu.pk>
2022-11-24 09:24:59 +0500

    added to test branch

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-const express  = require("express");
+const express = require("express");
 const path = require("path");
 
 const app = express();
 
-app.use("/static", express.static(path.resolve(__dirname,"frontend", "static")));
-app.get("/*", (req, res) => {
-    res.send(path.resolve("frontend", "index.html"));
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
 
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
 
-
-app.listen(process.env.PORT || 5060 , () => console.log("Server is running on port 5060"));
+app.listen(process.env.PORT || 3000, () => console.log("Server running..."));
